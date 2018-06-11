f8f70a688a40eb084e44bfb39e96dae1d15ceb66
Valentyn Vasylevskyy <vasylevskyyv@ryanair.com>
2018-06-11 18:01:07 +0300

    Profile Reducer & Get Current Profile (finished 46)

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
-import classnames from "classnames";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import TextFieldGroup from "../common/TextFieldGroup";
@@ -63,7 +62,7 @@ class Register extends Component {
                 Create your DevConnector account
               </p>
               <form onSubmit={this.onSubmit} noValidate>
-              <TextFieldGroup
+                <TextFieldGroup
                   placeholder="Name"
                   name="name"
                   value={this.state.name}

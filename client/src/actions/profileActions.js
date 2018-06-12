410d36bb866d535f0e7bd9918230a62ac1e8085e
Valentyn Vasylevskyy <vasylevskyyv@ryanair.com>
2018-06-12 13:53:19 +0300

    Dashboard & Profile State - Part 1 (finished 52)

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,16 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
 import {
   GET_PROFILE,
   PROFILE_LOADING,
-  CLEAR_CURRENT_PROFILE
-} from "./types";
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS
+} from './types';
 
-// GET current profile
+// Get current profile
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
   axios
-    .get("/api/profile")
+    .get('/api/profile')
     .then(res =>
       dispatch({
         type: GET_PROFILE,
@@ -21,6 +22,19 @@ export const getCurrentProfile = () => dispatch => {
       dispatch({
         type: GET_PROFILE,
         payload: {}
+      })
+    );
+};
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
       })
     );
 };

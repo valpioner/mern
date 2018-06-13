26f6d4b7ca80f05ba29fcc88c50c3c541e0477c3
Valentyn Vasylevskyy <vasylevskyyv@ryanair.com>
2018-06-13 14:02:20 +0300

    Prepeared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://valpioner:valpioner@ds227110.mlab.com:27110/mern_db',
-  secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }

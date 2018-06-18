f5c7ceb72cd95117941db892909cd1826147b708
Valentyn Vasylevskyy <vasylevskyyv@ryanair.com>
2018-06-18 18:19:27 +0300

    tweak colors

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -67,7 +67,7 @@ class Navbar extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-sm navbar-dark dark1">
         <div className="container">
           <Link className="navbar-brand" to="/">
             MyMaps 0.1

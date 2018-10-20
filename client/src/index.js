66d08996d24241bd0b8459c2be92174cf4d039d9
valen <valentun9@gmail.com>
2018-10-20 23:22:09 +0300

    added bootstrap css/js, commented out bootstrap css cdn link

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,13 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Added Bootstrap as deps (npm install jquery popper.js)
+//probably better to switch to react bootstrap components instead
+//https://blog.logrocket.com/how-to-use-bootstrap-with-react-a354715d1121
+//react-bootstrap || reactstrap
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
